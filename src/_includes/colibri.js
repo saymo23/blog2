8d443e0c89e1d67462c2bd4bcfbfa765bfbcf0cd
Daniel Santarriaga <daniel.santarriaga@gmail.com>
2022-01-14 13:05:34 -0700

    Blog: Colibri, incompleto. Se realizarar primero otra tarea de imagenes.

---
+++ src/_includes/colibri.js
@@ -0,0 +1,13 @@
+const colibri = document.getElementById('colibri')
+const c_layers = document.getElementsByClassName('layer_c')
+
+const colors = [
+  '#61946D', '#D0E562', '#B6D369', '#C2E812', '#91F5AD', '#59A96A',
+  '#06BCC1', '#12263A', '#65AFFF',
+  '#22ab24', '#cd0e66', '#18aa93', '#6f27cc', '#0f82f2', '#fd8c00'
+]
+
+for(let layer of c_layers){
+  //const random = Math.floor(Math.random() * 15);
+  //layer.style.fill = colors[random]
+}
