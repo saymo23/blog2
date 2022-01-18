a3bb686372f60c3370c8c2fd22784d1d90d6baaf
Daniel Santarriaga <daniel.santarriaga@gmail.com>
2022-01-18 14:48:57 -0700

    Blog: Colibri, completo. Se agrego el plugin para mostrar code

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,57 @@
 
 const { DateTime } = require("luxon");
 
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const Image = require("@11ty/eleventy-img");
+
+
+//Modulo de imagenes
+async function imageShortcode(src, alt, sizes = "100vw") {
+  if(alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ['webp', 'jpg']
+  });
+
+  let lowsrc  = metadata.jpeg[0];
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+  return `<picture>
+    ${Object.values(metadata).map(imageFormat => {
+      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+    }).join("\n")}
+      <img
+        src="${lowsrc.url}"
+        width="${highsrc.width}"
+        height="${highsrc.height}"
+        alt="${alt}"
+        loading="lazy"
+        decoding="async">
+    </picture>`;
+}
 
 module.exports = function (eleventyConfig) {
+  //Le indicamos a 11ty qwue carpetas o archivos exportara directamente a produccionb
   eleventyConfig.addPassthroughCopy('./src/style.css')
   eleventyConfig.addPassthroughCopy('./src/assets')
+  eleventyConfig.addPassthroughCopy('./img')
 
+  // Le indicamos a 11ty el usa de una funcion javascript para generar un filter
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_HUGE)
   })
+
+  //Le indicamos a 11ty que usaremos un hook para Nunjucks para el uso de imagenes
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+
+  //Le indicamos a 11ty la importacion de un plugin
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
--- src/_includes/colibri.js
+++ src/_includes/colibri.js
@@ -1,13 +1,13 @@
-const colibri = document.getElementById('colibri')
+//Obtenermos el arreglo de los elementos de cada pieza del tangram
 const c_layers = document.getElementsByClassName('layer_c')
-
+//Creamos una libreria de los colores que deseamos usar
 const colors = [
   '#61946D', '#D0E562', '#B6D369', '#C2E812', '#91F5AD', '#59A96A',
   '#06BCC1', '#12263A', '#65AFFF',
   '#22ab24', '#cd0e66', '#18aa93', '#6f27cc', '#0f82f2', '#fd8c00'
 ]
-
+//Le damos a cada pieza del tangram un color aleatorio
 for(let layer of c_layers){
-  //const random = Math.floor(Math.random() * 15);
-  //layer.style.fill = colors[random]
+  const random = Math.floor(Math.random() * colors.length);
+  layer.style.fill = colors[random]
 }
